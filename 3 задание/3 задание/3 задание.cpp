c34d8e5acab71a57438c9d8439b9c08bf07a0f65
Емельян Андреевич Егоров <113930312+Pomelkin@users.noreply.github.com>
2023-02-05 17:02:05 +0500

    Add files via upload

---
+++ 3 задание/3 задание/3 задание.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	ifstream FileIn ("File.txt");
+	vector <int> vec;
+	cout << FileIn.is_open();
+	while (!FileIn.eof())
+	{
+		int num;
+		FileIn >> num;
+		vec.push_back(num);
+	}
+	
+	for (auto i = vec.begin(); i < vec.end(); i++) 
+		cout << *i << " ";
+	cout << endl;
+	auto i = vec.begin();
+	bool Flag = 0;
+	while (Flag != 1)
+	{
+		Flag = 1;
+		for (i = vec.begin(); i < vec.end(); i++)
+			if (*i < 0 && *(i-1) != 0)
+			{
+				vec.insert(i, 0);
+				Flag = 0;
+				break;
+			}
+	}
+	for (auto i = vec.begin(); i < vec.end(); i++)
+		cout << *i << " ";
+	cout << endl;
+	FileIn.close();
+}
---
+++ 6/6/6.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <deque>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	ifstream FileIn("File.txt");
+	deque <int> deq;
+	while (!FileIn.eof())
+	{
+		int num;
+		FileIn >> num;
+		deq.push_back(num);
+	}
+
+	for (auto i = deq.begin(); i < deq.end(); i++)
+		cout << *i << " ";
+	cout << endl;
+
+	cout << "Введите K"<<endl;
+	int k;
+	cin >> k;
+	auto i = deq.begin();
+
+	deque <int> deq1 = deq;
+	bool Flag = 0;
+	while (Flag != 1)
+	{
+		Flag = 1;
+		for (i = deq.begin(); i < deq.end(); i++)
+ 			if (*i < k)
+			{
+				deq.erase(i);
+				Flag = 0;
+				break;
+			}
+		
+	}
+
+	for (auto i = deq.begin(); i < deq.end(); i++)
+		cout << *i << " ";
+	cout << endl;
+
+	cout << "Введите N\n";
+	int n,kol = 0;
+	cin >> n;
+
+	for (auto i = deq1.begin(); i < deq1.end(); i++)
+		if (*i == n) kol += 1;
+	cout << "Кол-во N: " << kol;
+	
+	FileIn.close();
+}
